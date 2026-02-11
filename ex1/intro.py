89940017387a04a9465f6fedbc30f801950a8d32
Ames7522 <aazogue@smith.edu>
2026-02-11 18:58:54 -0500

    Completed the basic descriptions/code for set up and cropping of images

--- ex1/intro.py
+++ ex1/intro.py
@@ -1,14 +1,53 @@
 '''
 Author: Ames Azogue Irigoyen
 Last Modified: February 11, 2026
-Description: This code .
+Description: This code takes an image and displays it in the viewer, experiments with different image filters.
 '''
-
-#Import Image class from PIL module
+# Getting started with images --------------------------------------------------------------
+# Imports Image class from PIL module
 from PIL import Image
 
-#Open image in rbg mode
+# Opens image in rbg mode
 im = Image.open(r"stars.jpg")           
 
 # Shows the image in image viewer
 im.show()   
+
+# Prints the format of the image (in this case JPEG)
+print(im.format)
+
+# Prints the size of the image (width, height)
+print(im.size)
+
+## Cropping the image ----------------------------------------------------------------------
+
+'''
+The following cropping is done by specifying the left, upper, right, and lower pixel coordinate.
+
+The left and upper coordinates are inclusive, while the right and lower coordinates are exclusive, the values can be interpreted as follows:
+
+left: The x-coordinate of the left edge of the crop area.
+upper: The y-coordinate of the top edge of the crop area.
+right: The x-coordinate of the right edge of the crop area.
+lower: The y-coordinate of the bottom edge of the crop area. 
+'''
+cropped_stars = im.crop((200, 1000, 700, 1500))
+cropped_stars.show()
+
+# To crop the image in the center, we can follow the following steps:
+width, height = im.size
+new_width, new_height = 700, 900 # example dimensions
+left = (width - new_width) / 2
+top = (height - new_height) / 2
+right = (width + new_width) / 2
+bottom = (height + new_height) / 2
+
+centered_stars = im.crop((left, top, right, bottom))
+centered_stars.show()
+
+# to save the cropped image, we can use the save() method
+centered_stars.save("centered_cropped_stars.jpg")
+cropped_stars.save("cropped_stars.jpg")
+
+# an alternative way to save an image comes in the cv2 module, which is part of the OpenCV library. This method is more efficient for saving images in certain formats, like PNG, and allows for more control over the compression settings. This will be covered in the section titled "OpenCV"
+
