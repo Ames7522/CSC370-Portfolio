d478d728da333ab614ccff93674650fda06c4835
Ames7522 <aazogue@smith.edu>
2026-02-13 18:29:23 -0500

    Finished a basic walkthrough code and the description file for ex 1

--- ex1/Ex1_Description.md
+++
@@ -1,1 +0,0 @@
-For Exercise 1, I want to fully set up my VSCode environment and start to play around with the images imported.
--- ex1/intro.py
+++ ex1/intro.py
@@ -1,7 +1,7 @@
 '''
 Author: Ames Azogue Irigoyen
 Last Modified: February 11, 2026
-Description: This code takes an image and displays it in the viewer, experiments with different image filters.
+Description: This code takes an image and displays it in the viewer, experiments with different basic image filters using pillow and CV2.
 '''
 # Getting started with images --------------------------------------------------------------
 # Imports Image class from PIL module
@@ -49,5 +49,48 @@ centered_stars.show()
 centered_stars.save("centered_cropped_stars.jpg")
 cropped_stars.save("cropped_stars.jpg")
 
+# converting to grayscale
+gray = im.convert("L")
+gray.show()
+
+# spliting the image into RGB channels
+r, g, b = im.split()
+r.show()
+g.show()
+b.show()
+
+
 # an alternative way to save an image comes in the cv2 module, which is part of the OpenCV library. This method is more efficient for saving images in certain formats, like PNG, and allows for more control over the compression settings. This will be covered in the section titled "OpenCV"
+
+#OpenCV ----------------------------------------------------------------------------------------------
+import cv2
+import numpy as np
+
+# Convert PIL image to OpenCV format
+im_cv = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+
+# Convert to grayscale using OpenCV
+gray_cv = cv2.cvtColor(im_cv, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Grayscale (OpenCV)", gray_cv)
+
+# Blurring and Edge Detection-------------------------------------------------------------------------
+
+# applying Gaussian blur using OpenCV
+gBlur = cv2.GaussianBlur(im_cv, (5, 5),0)
+cv2.imshow("Gaussian Blur (OpenCV)", gBlur)
+cv2.waitKey(0)  # Wait for a key press to close the windows
+cv2.destroyAllWindows() # Close all OpenCV windows
+
+#check the effect of a larger kernel size on the blurring of the image
+gBlur2 = cv2.GaussianBlur(im_cv, (51, 51),0) 
+cv2.imshow("Gaussian Blur (OpenCV)", gBlur2)
+cv2.waitKey(0)  # Wait for a key press to close the windows
+cv2.destroyAllWindows() # Close all OpenCV windows 
+
+# applying Canny edge detection using OpenCV
+edges = cv2.Canny(gray_cv, 100, 200)
+cv2.imshow("Edges", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
